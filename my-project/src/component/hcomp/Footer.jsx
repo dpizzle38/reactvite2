06cdaf87e502d8fe38fd5057ddb8beed0eca2263
dpizzle38 <130174020+dpizzle38@users.noreply.github.com>
2023-07-18 09:03:06 -0700

    still working on the footer component

--- my-project/src/component/hcomp/Footer.jsx
+++ my-project/src/component/hcomp/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
   return (
     <>
     <div className='w-[100%] h-[100%] md:w-[100%] md:h-[100%] bg-[#000]'>
-        <footer className='flex'>
-        <div>
-            <img src={logo} />
-            <p className='text-[#fff]'>Nura design headphones <br /> tuned to you. Bringing you <br /> closer to music with <br /> personalized sound.</p>
+        <footer className='flex gap-[8rem]'>
+        <div className='leadinng-8'>
+            <img className='ml-[2rem] pt-[3rem]' src={logo} />
+            <p className='text-[#fff] ml-[2rem] pt-[1.5rem]'>Nura design headphones <br /> tuned to you. Bringing you <br /> closer to music with <br /> personalized sound.</p>
         </div>
 
         
-         <ul className=''>
+         <ul className='pt-[3rem] leading-8'>
                 <li className='text-[#737373]'>SHOP</li>
                 <li className='text-[#fff]'><a href="#">NuraTrue Pro</a></li>
                 <li className='text-[#fff]'><a href="#">Audio Transmitter</a></li>
@@ -25,7 +25,7 @@ const Footer = () => {
                 <li className='text-[#fff]'><a href="#">Subscription</a></li>
          </ul>
         
-        <ul>
+        <ul className='pt-[3rem] leading-8'>
             <li className='text-[#737373]'>Info</li>
             <li className='text-[#fff]'><a href="#">Why Nura ?</a></li>
             <li className='text-[#fff]'><a href="#">Shipping</a></li>
@@ -35,7 +35,7 @@ const Footer = () => {
            
         </ul>
 
-        <ul>
+        <ul className='pt-[3rem] leading-8'>
             <li className='text-[#737373]'>SUPPORT</li>
             <li className='text-[#fff]'> <a href="#">Help Centre</a></li>
             <li className='text-[#fff]'> <a href="#">Contact Us</a></li>
@@ -44,11 +44,11 @@ const Footer = () => {
         <ul>
             <li className='text-[#737373]'>SOCIALS</li>
             <li className='text-[#fff]'><a href="#">Instagram</a></li>
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">YouTube</a></li>
-            <li><a href="#">Tidal</a></li>
-            <li></li>
-            <li></li>
+            <li className='text-[#fff]'><a href="#">Facebook</a></li>
+            <li className='text-[#fff]'><a href="#">YouTube</a></li>
+            <li className='text-[#fff]'><a href="#">Tidal</a></li>
+            <li className='text-[#fff]'><a href="#">Twitter</a></li>
+            <li className='text-[#fff]'><a href="#">Discord</a></li>
         </ul>
         </footer>
     </div>
